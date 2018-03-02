8fff097546671cd9b29a1599c83f52ff62bd52f0
cemolcay <ccemolcay@gmail.com>
2018-03-03 01:21:43 +0300

    Library setup

---
+++ AUSequencer/Bridge.h
@@ -0,0 +1,17 @@
+//
+//  Bridge.h
+//  AUSequencer
+//
+//  Created by Cem Olcay on 3.03.2018.
+//  Copyright © 2018 cemolcay. All rights reserved.
+//
+
+#ifndef Bridge_h
+#define Bridge_h
+
+#include "ABLLink.h"
+#include "ABLLinkUtils.h"
+#include "ABLLinkSettingsViewController.h"
+#import "Audiobus.h"
+
+#endif /* Bridge_h */
---
+++ Podfile
@@ -0,0 +1,5 @@
+target 'AUSequencer' do
+  use_frameworks!
+  pod 'AudioKit'
+  pod 'Audiobus'
+end
